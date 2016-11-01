6060f0ed60326a64625a73e0be55fd2029f73d8e
江丁魁 <546759734@qq.com>
2016-11-01 16:48:19 +0800

    项目初始结构

---
+++ app/src/main/java/com/esint/esintmanager/Common/Constant.java
@@ -0,0 +1,5 @@
+package com.esint.esintmanager.Common;
+
+public class Constant {
+
+}
---
+++ app/src/main/java/com/esint/esintmanager/MyApplication/MyApplication.java
@@ -0,0 +1,8 @@
+package com.esint.esintmanager.MyApplication;
+
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+}
